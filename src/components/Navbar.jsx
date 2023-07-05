7985fe5f5841a8164b1fcf5d8aade2aed45e4d84
ppinng <thiti.saelee@mail.kmutt.ac.th>
2023-07-05 22:36:45 +0700

    [ADD] NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import Logo from "../assets/Web_Logo.png";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    // <div className="max-w-full border  flex items-center invisible lg:visible">
+    //   <div className="pl-1">
+    //     <img src={Logo} alt="Website logo"  />
+    //   </div>
+    //   <div>
+    //     <h1 className="font-Monsterrat text-2xl font-bold">Thiti.</h1>
+    //   </div>
+    //   <div>Home</div>
+    //   <div>About Me</div>
+    //   <div>Projects</div>
+    //   <div>Contact</div>
+    // </div>
+    <nav className=" max-w-full max-sm:px-7 px-60px py-2 mx-auto shadow-xl bg-white">
+      <div className="flex justify-between items-center ">
+        <div className="flex justify-between items-center ">
+          <a href="/">
+            <img src={Logo} alt="logo" width={70} height={100} />
+          </a>
+          <div className="font-quicksand text-2xl font-bold pt-3 pl-2">Thiti.</div>
+        </div>
+        <div className="hidden sm:flex">
+          <ul className="hidden sm:flex gap-2">
+            <li className="mr-8">
+              <a
+                href="/"
+                className="font-quicksand text-xl hover:border-b-4 hover:border-blue-300"
+              >
+                Home
+              </a>
+            </li>
+            <li className="mr-8">
+              <a
+                href="/"
+                className="font-quicksand text-xl hover:border-b-4 hover:border-blue-300"
+              >
+                About
+              </a>
+            </li>
+            <li className="mr-8">
+              <a
+                href="/"
+                className="font-quicksand text-xl hover:border-b-4 hover:border-blue-400"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div onClick={handleNav} className="sm:hidden cursor-pointer pt-3">
+          <FaBars size={25} />
+        </div>
+      </div>
+      <div
+        className={
+          nav
+            ? "fixed right-0 top-0 w-48 sm:hidden h-screen bg-white p-10 ease-in-out duration-500 shadow-lg"
+            : "fixed right-[-100%] top-0 w-48 sm:hidden h-screen bg-white p-10 ease-in-out duration-500 shadow-lg"
+        }
+      >
+        <div className="flex w-full items-center justify-end ml-3 mb-4">
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+        <ul className="flex flex-col gap-9 mt-10 ">
+          <li>
+            <a
+              href="/"
+              onClick={() => setNav(false)}
+              className="font-quicksand text-xl hover:border-b-4 hover:border-red-300"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              onClick={() => setNav(false)}
+              className="font-quicksand text-xl hover:border-b-4 hover:border-red-300"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              onClick={() => setNav(false)}
+              className="font-quicksand text-xl hover:border-b-4 hover:border-red-300"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <img src={Logo} alt="logo" className="h-24 w-24 mt-10" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
