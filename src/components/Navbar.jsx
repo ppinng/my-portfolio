e00cc969d99ba087ad09f7fafa273ca4d2212c45
ppinng <thiti.saelee@mail.kmutt.ac.th>
2023-07-23 22:45:22 +0700

    [FIX] Suk tee

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Logo} from "../assets/Web_logo.png";
+import Logo from "../assets/Web_logo.png";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
