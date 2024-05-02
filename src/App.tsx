07c2d52e2e8ed6f7940717f8c4c9c56f645be088
ppinng <thiti.saelee@mail.kmutt.ac.th>
2024-05-03 01:16:39 +0700

    [UPD] Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "../src/components/Navbar";
 import HomePage from "./pages/HomePage";
 import Aboutme from "./pages/Aboutme";
 import Myworks from "./pages/Mywork";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
 function App() {
@@ -12,14 +12,14 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter >
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/aboutme" element={<Aboutme />} />
         <Route path="/my-projects" element={<Myworks />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter >
   );
 }
 
