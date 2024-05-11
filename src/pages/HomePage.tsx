94429e381e19802079e2a15d3b03b6cf2c1a92b5
ppinng <thiti.saelee@mail.kmutt.ac.th>
2024-05-11 20:31:44 +0700

    Update SEO

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   useEffect(() => {
+      document.title = "Home";
     ReactGA.send({
       hitType: "pageview",
       page: window.location.pathname,
